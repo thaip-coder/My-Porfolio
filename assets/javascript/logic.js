b70b177e0606f0b003e10bd27fb11a80ffe47eb1
Thai Phan <thaiphan90@yahoo.com>
2019-07-27 00:59:50 -0500

    updating images

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,42 @@
 $(document).ready(function(){
-
     $('.parallax').parallax();
     $('.collapsible').collapsible();
-    $('.carousel').carousel();
+
+    $("#bio").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#collap-head-me").offset().top
+          }, 600);
+    });
+
+    $("#projects").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#collap-head-projects").offset().top
+          }, 600);
+    });
+
+    $("#competitive").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#cr").offset().top
+          }, 600);
+    });
+
+    $("#top").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#nav-bg").offset().top
+          }, 600);
+    });
+
+    $("#collap-head-me").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#collap-head-me").offset().top
+          }, 600);
+    });
+
+    $("#collap-head-projects").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#collap-head-projects").offset().top
+          }, 600);
+    });
+
   });
 
