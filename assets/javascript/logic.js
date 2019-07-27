8e2b96a7be0d129de81cca49731b0321c488a8be
Thai Phan <thaiphan90@yahoo.com>
2019-07-27 01:02:29 -0500

    updating text

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,33 @@
 $(document).ready(function(){
+
+    /* ---------- Initiate Parallax ---------- */
     $('.parallax').parallax();
+
+    /* ---------- Initiate Collapsible ---------- */
     $('.collapsible').collapsible();
 
+    /* ---------- Scroll to About Me ---------- */
     $("#bio").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#collap-head-me").offset().top
           }, 600);
     });
 
+    /* ---------- Scroll to Projects ---------- */
     $("#projects").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#collap-head-projects").offset().top
           }, 600);
     });
 
+    /* ---------- Scroll to Footer ---------- */
     $("#competitive").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#cr").offset().top
           }, 600);
     });
 
+    /* ---------- Scroll back to Navbar ---------- */
     $("#top").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#nav-bg").offset().top
