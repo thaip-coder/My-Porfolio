3b2ec361f08679ab48e21d586cefe5d9d4fa1145
Thai Phan <thaiphan90@yahoo.com>
2019-09-29 23:24:10 -0500

    java change

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,22 +6,32 @@ $(document).ready(function(){
     /* ---------- Initiate Collapsible ---------- */
     $('.collapsible').collapsible();
 
-    /* ---------- Scroll to About Me ---------- */
-    $("#bio").on("click", function() {
+    /* ---------- Initiate Floating Button ----------*/
+      $('.fixed-action-btn').floatingActionButton();
+
+    /* ---------- Scroll to Contact ---------- */
+    $("#contact").on("click", function() {
         $('html, body').animate({
-            scrollTop: $("#collap-head-me").offset().top
+            scrollTop: $("#collap-head-contact").offset().top
           }, 600);
     });
 
     /* ---------- Scroll to Projects ---------- */
-    $("#projects").on("click", function() {
+    $("#personal-proj").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#collap-head-projects").offset().top
           }, 600);
     });
 
+    /* ---------- Scroll to Group Projects ---------- */
+    $("#group-proj").on("click", function() {
+      $('html, body').animate({
+          scrollTop: $("#collap-head-group").offset().top
+        }, 600);
+    });
+
     /* ---------- Scroll to Footer ---------- */
-    $("#competitive").on("click", function() {
+    $("#info").on("click", function() {
         $('html, body').animate({
             scrollTop: $("#cr").offset().top
           }, 600);
@@ -34,18 +44,49 @@ $(document).ready(function(){
           }, 600);
     });
 
-    /* ---------- Focuses About Me ----------*/
+    /* ---------- Focuses Group Projects ----------*/
     $("#collap-head-group").on("click", function() {
-        $('html, body').animate({
+        if ($("#collap-head-group").hasClass("inactive")) {
+          $('html, body').animate({
             scrollTop: $("#collap-head-group").offset().top
           }, 600);
+          $("#collap-head-group").removeClass("inactive");
+          $("#collap-head-group").addClass("active");
+        } else 
+        if ($("#collap-head-group").hasClass("active")) {
+          $("#collap-head-group").removeClass("active");
+          $("#collap-head-group").addClass("inactive");
+        };
     });
 
     /* ---------- Focuses Projects ----------*/
     $("#collap-head-projects").on("click", function() {
+      if ($("#collap-head-projects").hasClass("inactive")) {
         $('html, body').animate({
-            scrollTop: $("#collap-head-projects").offset().top
-          }, 600);
+          scrollTop: $("#collap-head-projects").offset().top
+        }, 600);
+        $("#collap-head-projects").removeClass("inactive");
+        $("#collap-head-projects").addClass("active");
+      } else 
+      if ($("#collap-head-projects").hasClass("active")) {
+        $("#collap-head-projects").removeClass("active");
+        $("#collap-head-projects").addClass("inactive");
+      };
+    });
+
+    /* ---------- Focuses Contact Form ----------*/
+    $("#collap-head-contact").on("click", function() {
+      if ($("#collap-head-contact").hasClass("inactive")) {
+        $('html, body').animate({
+          scrollTop: $("#collap-head-contact").offset().top
+        }, 600);
+        $("#collap-head-contact").removeClass("inactive");
+        $("#collap-head-contact").addClass("active");
+      } else 
+      if ($("#collap-head-contact").hasClass("active")) {
+        $("#collap-head-contact").removeClass("active");
+        $("#collap-head-contact").addClass("inactive");
+      };
     });
 
   });
