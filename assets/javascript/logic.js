5b45f394887e347f18026dada50baf7a16e02c31
Thai Phan <thaiphan90@yahoo.com>
2020-01-20 21:20:54 -0600

    added tooltips

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    /* ---------- Initiates Tooltips --------- */
+    $(document).ready(function(){
+      $('.tooltipped').tooltip();
+    });  
+
     /* ---------- Initiate Parallax ---------- */
     $('.parallax').parallax();
 
